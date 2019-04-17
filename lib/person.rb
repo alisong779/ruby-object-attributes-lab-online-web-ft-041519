bde3bda7f7bab54b23089229a954853643d5b915
Alison Golden <alisong779@yahoo.com>
2019-04-17 23:20:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,23 +1,19 @@
 class Person 
-  def initialize(name, job)
-    @name = name
-    @job = job
-  end 
   
-  def name 
-    @name 
+ def name 
+    @name
+  end 
+ 
+  def job
+    @job
   end 
   
   def name=(new_name)
     @name = new_name
-  end 
-
-  def job 
-    @job 
-  end 
+  end
   
   def job=(new_job)
     @job = new_job
   end 
-
+  
 end 
