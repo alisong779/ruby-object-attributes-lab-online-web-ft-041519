96e67d6dfe70faf244f02702a817bc33b8c44b6c
Alison Golden <alisong779@yahoo.com>
2019-04-17 22:20:58 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog
+  def initialize (dogs_name, dogs_breed)
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name=(dogs_name, dogs_breed)
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def dogs_name
+    @name
+  end
+  def dogs_breed
+    @breed
+  end 
+end 
+
+# def initialize(name)
+#     @name = name
+#   end
+ 
+#   def name
+#     @name
+#   end
+ 
+#   def name=(new_name)
+#     @name = new_name
+#   end
+
